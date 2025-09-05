40f9aeb1c4a6cdee7f2bc531d579b78245041794
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-05 19:57:01 +0100

    Add .gitignore to exclude node_modules

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.{html,js}",        // look at root files (like 1-index.html)
+    "./src/**/*.{html,js}"  // look inside src/ for html/js
+  ],
   theme: {
     extend: {},
   },
